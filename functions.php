5433e804330170ad5dc21e72b91f0c919f313e6e
SilenceCr1stal <nikitapolyashov2@gmail.com>
2020-12-07 22:41:15 +0300

    v2.2

--- functions.php
+++ functions.php
@@ -1,28 +1,29 @@
 <?php
-    function register_styles() {
-        wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
+    function setUp_styles() {
+        wp_register_style('style', get_template_directory_uri() . '/style.css');
+        wp_enqueue_style('style');
 
-        wp_enqueue_style('media-styles', get_template_directory_uri() . '/assets/css/media.css');
+        wp_register_style('media-styles', get_template_directory_uri() . '/assets/css/media.css');
+        wp_enqueue_style('media-styles');
     }
 
-add_action('wp_enqueue_scripts', 'register_styles');
+add_action('wp_enqueue_scripts', 'setUp_styles');
 
-    function upload_scripts() {
-        // wp_deregister_script('script');
+    function setUp_scripts() {
+        wp_register_script('script', get_template_directory_uri() . '/assets/js/script.js');
+        wp_enqueue_script('script');
 
-        wp_register_script('script-style', get_template_directory_uri() .'/assets/js/script.js');
-        wp_enqueue_script('script-style');
+        // wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/script.js');
 
-        wp_register_script('SmoothScrolling', get_template_directory_uri() .'/assets/js/SmoothScroll.js', array('script-style'), null, false);
-        wp_enqueue_script('SmoothScrolling');
+        wp_register_script('smooth-scrolling', get_template_directory_uri() . '/assets/js/SmoothScroll.js');
+        wp_enqueue_script('smooth-scrolling');
         
-        function defer_parsing_of_js ( $url ) {
-            if ( FALSE === strpos( $url, '.js' ) ) return $url;
-            if ( strpos( $url, 'jquery.js' ) ) return $url;
-            return "$url' defer ";
-            }
-            add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
-
+        function defer_parsing_js($url) {
+            if (FALSE === strpos($url, '.js')) return $url;
+            if (strpos($url, 'jquery.js')) return $url;
+            return "$url' defer '";
+        }
+        add_filter('clean_url', 'defer_parsing_js');
     }
 
-add_action('wp_enqueue_scripts', 'upload_scripts');
+add_action('wp_enqueue_scripts', 'setUp_scripts');
